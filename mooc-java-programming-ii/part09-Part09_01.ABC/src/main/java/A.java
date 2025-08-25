f445b986255dcc3c4139efa8103bd30b0fb6bf86
vedgharat <gved.16@gmail.com>
2025-08-25 21:34:17 +0530

    9.1

---
+++ mooc-java-programming-ii/part09-Part09_01.ABC/src/main/java/A.java
@@ -0,0 +1,11 @@
+
+public class A {
+    
+    
+    public void a(){
+        
+        System.out.println("A");
+        
+    }
+    
+}
---
+++ mooc-java-programming-ii/part09-Part09_01.ABC/src/main/java/B.java
@@ -0,0 +1,8 @@
+public class B extends A {
+    
+    public void b(){
+         System.out.println("B");
+        
+    }
+    
+}

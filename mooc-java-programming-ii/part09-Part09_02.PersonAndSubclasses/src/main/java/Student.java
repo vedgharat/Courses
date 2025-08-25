b6e11521928ababcafb1f2ffc528ab6dabc85d67
vedgharat <gved.16@gmail.com>
2025-08-25 21:53:39 +0530

    9.2

---
+++ mooc-java-programming-ii/part09-Part09_02.PersonAndSubclasses/src/main/java/Student.java
@@ -0,0 +1,33 @@
+public class Student extends Person {
+    
+    private int credits;
+
+    public Student(String name, String address) {
+
+        super(name, address);
+        this.credits = 0;
+    }
+    
+    
+  public int credits(){
+      
+      return this.credits;
+  }
+    
+    public void study(){
+        
+        credits++;
+    }
+    
+    
+    @Override
+    public String toString(){
+        
+        
+        return super.toString() + " \n" +"  Study credits " + this.credits;
+    }
+    
+    
+    
+
+}

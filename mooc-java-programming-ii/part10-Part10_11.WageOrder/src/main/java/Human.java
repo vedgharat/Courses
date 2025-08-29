9a057aefca412427843294b19d8f43b1a634e044
vedgharat <gved.16@gmail.com>
2025-08-29 23:48:21 +0530

    kn

--- mooc-java-programming-ii/part10-Part10_11.WageOrder/src/main/java/Human.java
+++ mooc-java-programming-ii/part10-Part10_11.WageOrder/src/main/java/Human.java
@@ -1,5 +1,5 @@
 
-public class Human {
+public class Human implements Comparable<Human> {
 
     private int wage;
     private String name;
@@ -17,9 +17,22 @@ public class Human {
         return wage;
     }
 
-
     @Override
     public String toString() {
         return name + " " + wage;
+
     }
+
+    @Override
+    public int compareTo(Human obj) {
+
+        if (this.getWage() == obj.wage) {
+            return 0;
+
+        }
+
+        return obj.getWage() - this.getWage();
+
+    }
+
 }

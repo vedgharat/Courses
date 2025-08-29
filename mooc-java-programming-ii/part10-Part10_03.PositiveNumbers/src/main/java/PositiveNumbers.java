ac594f8fff5cb81ab917352e6fea82350b6b26c9
vedgharat <gved.16@gmail.com>
2025-08-29 22:49:10 +0530

    ugkgk

--- mooc-java-programming-ii/part10-Part10_03.PositiveNumbers/src/main/java/PositiveNumbers.java
+++ mooc-java-programming-ii/part10-Part10_03.PositiveNumbers/src/main/java/PositiveNumbers.java
@@ -1,4 +1,5 @@
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 import java.util.stream.Collectors;
@@ -11,4 +12,22 @@ public class PositiveNumbers {
 
     }
 
+    
+    
+    public static List<Integer> positive(List<Integer> numbers){
+        
+        
+        ArrayList<Integer> getPosNumbers = numbers.stream()
+                .filter(i -> i > 0)
+                .collect(Collectors.toCollection(ArrayList::new ));
+                
+                return getPosNumbers;
+        
+        
+        
+        
+        
+    }
+    
+    
 }

d49058997d611d45a627dfb1220c6695c66e3764
vedgharat <gved.16@gmail.com>
2025-08-29 23:30:25 +0530

    dfw

--- mooc-java-programming-ii/part10-Part10_05.PrintingUserInput/src/main/java/PrintingUserInput.java
+++ mooc-java-programming-ii/part10-Part10_05.PrintingUserInput/src/main/java/PrintingUserInput.java
@@ -7,5 +7,22 @@ public class PrintingUserInput {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        ArrayList<String> userInputList = new ArrayList<>();
+
+        while (true) {
+            String userInput = scanner.nextLine();
+
+            if (userInput.isEmpty()) {  // i used isBlank() here but it failed the test
+
+                userInputList.stream()
+                        .forEach(word -> System.out.println(word));
+
+                break;
+            }
+
+            userInputList.add(userInput);
+
+        }
+
     }
 }

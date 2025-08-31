de06c6d58a26f79cc6afc3fe2e0b1c798865f0cc
vedgharat <gved.16@gmail.com>
2025-08-31 15:02:56 +0530

    trj

---
+++ mooc-java-programming-ii/part11-Part11_03.ShowAndTicket/src/main/java/Show.java
@@ -0,0 +1,20 @@
+public class Show {
+    
+    
+    private String movie;
+    
+    private String time;
+
+    
+   
+    
+   
+    
+    
+    
+    
+   
+    
+    
+    
+}
---
+++ mooc-java-programming-ii/part11-Part11_03.ShowAndTicket/src/main/java/Ticket.java
@@ -0,0 +1,13 @@
+public class Ticket {
+    
+    private int seat;
+    
+    private int code;
+    
+    private Show shows;    //i treid arrayList<show> and it didn't work.
+
+  
+    
+  
+    
+}

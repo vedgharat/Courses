1cbc9bf2b41e7032ee1189377857a2b5de5a0c8a
vedgharat <gved.16@gmail.com>
2025-08-31 18:19:16 +0530

    hjhk

---
+++ mooc-java-programming-ii/part11-Part11_05.ThePlayerAndTheBot/src/main/java/Bot.java
@@ -0,0 +1,17 @@
+public class Bot extends Player{
+    
+    
+    
+    
+    @Override
+    public void play(){
+        
+    }
+    
+    public void addMove(String move){
+        
+        
+    }
+    
+    
+}
---
+++ mooc-java-programming-ii/part11-Part11_05.ThePlayerAndTheBot/src/main/java/Player.java
@@ -0,0 +1,18 @@
+public class Player {
+    
+    
+    private String name;
+    
+    
+    
+    public void play(){
+        
+        
+    }
+    
+    public void printName(){
+        
+        
+    }
+    
+}

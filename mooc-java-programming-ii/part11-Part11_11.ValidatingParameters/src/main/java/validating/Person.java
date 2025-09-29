ea7a04840faf1f681d1602cf6f62ceda05d7d880
vedgharat <gved.16@gmail.com>
2025-09-29 23:40:52 +0530

    11hikh

--- mooc-java-programming-ii/part11-Part11_11.ValidatingParameters/src/main/java/validating/Person.java
+++ mooc-java-programming-ii/part11-Part11_11.ValidatingParameters/src/main/java/validating/Person.java
@@ -1,3 +1,4 @@
+
 package validating;
 
 public class Person {
@@ -6,7 +7,11 @@ public class Person {
     private int age;
 
     public Person(String name, int age) {
-
+    	if (name == null || name.isEmpty() || name.length() > 40) {
+			throw new IllegalArgumentException("Name must not be empty, null or greater than 40 characters!");
+		} else if ( age < 0 || age > 120) {
+			throw new IllegalArgumentException("age out of range");
+		}
         this.name = name;
         this.age = age;
     }
